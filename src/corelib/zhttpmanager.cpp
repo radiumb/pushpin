c15c10590458b70a6a5aa5ee4c3487f3fa6ce5bb
Maksym <mtakchuk86@gmail.com>
2022-12-09 12:30:02 +0200

    updates for ws subscribe cache

--- src/corelib/zhttpmanager.cpp
+++ src/corelib/zhttpmanager.cpp
@@ -620,7 +620,7 @@ DELETE_OLD_SUBSCRIPTION_ITEMS:
 			ZhttpRequestPacket tempPacket = packet;
 			tempPacket.ids[0].id = gClosedClientList[0].id;
 			tempPacket.type = ZhttpRequestPacket::KeepAlive;
-			tmpbuf = QByteArray("T") + TnetString::fromVariant(tempPacket.toVariant());
+			tempbuf = QByteArray("T") + TnetString::fromVariant(tempPacket.toVariant());
 
 			if(log_outputLevel() >= LOG_LEVEL_DEBUG)
 				LogUtil::logVariantWithContent(LOG_LEVEL_DEBUG, tempPacket.toVariant(), "body", "%s client: OUT %s", logprefix, instanceAddress.data());
