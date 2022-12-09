9d6786c0b87acedff26e373b77394e56cc08be88
Maksym <mtakchuk86@gmail.com>
2022-12-09 15:16:58 +0200

    updates for ws subscribe cache

--- src/corelib/zhttpmanager.cpp
+++ src/corelib/zhttpmanager.cpp
@@ -1192,6 +1192,24 @@ public slots:
 								if (gClosedClientList[i].id == p.ids[0].id)
 								{
 									log_debug("[CACHE] Cancel subscription sending to client id=%s credit=%d", (const char *)p.ids[0].id, hdata.value("body").toByteArray().size());
+
+									ZhttpRequestPacket tempPacket;
+									tempPacket.type = ZhttpRequestPacket::Credit;
+									tempPacket.credit = hdata.value("body").toByteArray().size();
+									tempPacket.from = receiver.data();
+									tempPacket.ids[0].id = p.ids[0].id;
+									tempPacket.ids[0].seq = -1;
+									QByteArray tempbuf = QByteArray("T") + TnetString::fromVariant(tempPacket.toVariant());
+
+									if(log_outputLevel() >= LOG_LEVEL_DEBUG)
+										LogUtil::logVariantWithContent(LOG_LEVEL_DEBUG, tempPacket.toVariant(), "body", "%s client: OUT %s", logprefix, instanceAddress.data());
+
+									QList<QByteArray> msg;
+									msg += p.from;
+									msg += QByteArray();
+									msg += tempbuf;
+									client_out_stream_sock->write(msg);
+
 									return;
 								}
 								
@@ -1335,6 +1353,22 @@ public slots:
 					if (gClosedClientList[i].id == p.ids[0].id)
 					{
 						log_debug("[CACHE] Cancel sending to client id=%s credit=%d", (const char *)p.ids[0].id, hdata.value("body").toByteArray().size());
+						ZhttpRequestPacket tempPacket;
+						tempPacket.type = ZhttpRequestPacket::Credit;
+						tempPacket.credit = hdata.value("body").toByteArray().size();
+						tempPacket.from = receiver.data();
+						tempPacket.ids[0].id = p.ids[0].id;
+						tempPacket.ids[0].seq = -1;
+						QByteArray tempbuf = QByteArray("T") + TnetString::fromVariant(tempPacket.toVariant());
+
+						if(log_outputLevel() >= LOG_LEVEL_DEBUG)
+							LogUtil::logVariantWithContent(LOG_LEVEL_DEBUG, tempPacket.toVariant(), "body", "%s client: OUT %s", logprefix, instanceAddress.data());
+
+						QList<QByteArray> msg;
+						msg += p.from;
+						msg += QByteArray();
+						msg += tempbuf;
+						client_out_stream_sock->write(msg);
 						return;
 					}
 					
