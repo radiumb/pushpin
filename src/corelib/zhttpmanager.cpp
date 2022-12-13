d351aa4ecc0abd96ddd311c0e75b9ee87a526189
Maksym <mtakchuk86@gmail.com>
2022-12-13 16:42:07 +0200

    updates for ws subscribe cache

--- src/corelib/zhttpmanager.cpp
+++ src/corelib/zhttpmanager.cpp
@@ -819,15 +819,15 @@ DELETE_OLD_SUBSCRIPTION_ITEMS:
 			long cacheByteCount = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
 			shm_read_count += 4; // for cacheItemMaxSizeKbytes
 			int cacheItemMaxCount = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
-			log("asdfasdfasdfasdf cacheItemMaxCount=%d", cacheItemMaxCount);
+			log_debug("asdfasdfasdfasdf cacheItemMaxCount=%d", cacheItemMaxCount);
 			if (cacheItemMaxCount <= 0) cacheItemMaxCount = 64;		// default
 
 			int cacheTimeoutSeconds = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
-			log("asdfasdfasdfasdf cacheTimeoutSeconds=%d", cacheTimeoutSeconds);
+			log_debug("asdfasdfasdfasdf cacheTimeoutSeconds=%d", cacheTimeoutSeconds);
 			if (cacheTimeoutSeconds <= 0) cacheTimeoutSeconds = 5;	// default
 
 			int cacheMethodCount = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
-			log("asdfasdfasdfasdf cacheMethodCount=%d", cacheMethodCount);
+			log_debug("asdfasdfasdfasdf cacheMethodCount=%d", cacheMethodCount);
 			
 
 			// cache subscribe method
@@ -835,15 +835,15 @@ DELETE_OLD_SUBSCRIPTION_ITEMS:
 			shm_read_count += 4; // cache subscribe byte count
 			shm_read_count += 4; // for cacheSubscribeItemMaxSizeKbytes
 			int cacheSubscribeItemMaxCount = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
-			log("asdfasdfasdfasdf cacheSubscribeItemMaxCount=%d", cacheSubscribeItemMaxCount);
+			log_debug("asdfasdfasdfasdf cacheSubscribeItemMaxCount=%d", cacheSubscribeItemMaxCount);
 			if (cacheSubscribeItemMaxCount <= 0) cacheSubscribeItemMaxCount = 256;		// default
 
 			int cacheSubscribeTimeoutSeconds = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
-			log("asdfasdfasdfasdf cacheSubscribeTimeoutSeconds=%d", cacheSubscribeTimeoutSeconds);
+			log_debug("asdfasdfasdfasdf cacheSubscribeTimeoutSeconds=%d", cacheSubscribeTimeoutSeconds);
 			if (cacheSubscribeTimeoutSeconds <= 0) cacheSubscribeTimeoutSeconds = 3600*4;	// default
 
 			int cacheSubscribeMethodCount = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
-			log("asdfasdfasdfasdf cacheSubscribeMethodCount=%d", cacheSubscribeMethodCount);
+			log_debug("asdfasdfasdfasdf cacheSubscribeMethodCount=%d", cacheSubscribeMethodCount);
 			
 			// delete old cache items
 			deleteOldCacheItem(cacheTimeoutSeconds, cacheItemMaxCount);
@@ -1268,14 +1268,14 @@ public slots:
 				shm_read_count = 200 + groupByteCount;
 				long cacheByteCount = *(long *)&shm_str[shm_read_count]; shm_read_count += 4; // for cacheByteCount
 				int cacheItemMaxSizeKbytes = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
-				log("asdfasdfasdfasdf cacheItemMaxSizeKbytes=%d", cacheItemMaxSizeKbytes);
+				log_debug("asdfasdfasdfasdf cacheItemMaxSizeKbytes=%d", cacheItemMaxSizeKbytes);
 				if (cacheItemMaxSizeKbytes <= 0) cacheItemMaxSizeKbytes = 8;
 
 				// Cache Subscription
 				shm_read_count = 200 + groupByteCount + cacheByteCount;
 				shm_read_count += 4; // for cacheByteCount
 				int cacheSubscriptionItemMaxSizeKbytes = *(long *)&shm_str[shm_read_count]; shm_read_count += 4;
-				log("asdfasdfasdfasdf cacheSubscriptionItemMaxSizeKbytes=%d", cacheSubscriptionItemMaxSizeKbytes);
+				log_debug("asdfasdfasdfasdf cacheSubscriptionItemMaxSizeKbytes=%d", cacheSubscriptionItemMaxSizeKbytes);
 				if (cacheSubscriptionItemMaxSizeKbytes <= 0) cacheSubscriptionItemMaxSizeKbytes = 128;
 				
 				for (int i = 0; i < cacheListCount; i++)
