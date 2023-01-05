7ee9d8ddef41daa91de7214bf544a7eeabfd8039
Maksym <mtakchuk86@gmail.com>
2023-01-05 11:15:37 +0200

    fixing some issues

--- src/proxy/app.cpp
+++ src/proxy/app.cpp
@@ -560,7 +560,7 @@ public:
 		log_info("started");
 
 		log_info("starting wscat child process");
-/*
+
 		pid_t pid;
 		pid = fork();
 		if (pid == -1){
@@ -585,7 +585,6 @@ public:
 			
 			exit(0);
 		}
-*/
 	}
 
 private slots:
