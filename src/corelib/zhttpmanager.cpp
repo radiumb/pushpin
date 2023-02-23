b572d932dda551b5d2217c4f1f03505cf9bba2c7
Maksym Tkachuk <mtkachuk86@gmail.com>
2023-02-23 15:50:38 +0200

    Update zhttpmanager.cpp

--- src/corelib/zhttpmanager.cpp
+++ src/corelib/zhttpmanager.cpp
@@ -1826,7 +1826,7 @@ public slots:
 				{
 					goto ZWS_CLIENT_IN_WRITE;
 				}
-
+/*
 				// if mult-part response ?
 				if (p.more == true)
 				{
@@ -1839,7 +1839,7 @@ public slots:
 					p.type = ZhttpResponsePacket::KeepAlive;
 					goto ZWS_CLIENT_IN_WRITE;
 				}
-
+*/
 				int cacheItemCount = gCacheItemList.count();
 				for (int i = 0; i < cacheItemCount; i++)
 				{
