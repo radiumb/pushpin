e0e55e110bc544b00fddc3634692a3522ba6fdb6
Maksym Tkachuk <mtkachuk86@gmail.com>
2023-04-11 17:38:08 +0300

    Update zhttpmanager.cpp

--- src/corelib/zhttpmanager.cpp
+++ src/corelib/zhttpmanager.cpp
@@ -38,6 +38,7 @@
 #include <QJsonObject>
 #include <QFile>
 #include <QFileInfo>
+#include <QDir>
 #include <QStringList>
 #include <QHash>
 #include <QPointer>
@@ -1180,8 +1181,7 @@ public:
 				log_debug("[CACHEITEM] Sending restart pushpin");
 
 				// save log file
-				QDir tdir;
-				QString prometheusBackupFile = tdir.tempPath() + "/prometheus_backup.bin";
+				QString prometheusBackupFile = QDir::tempPath() + "/prometheus_backup.bin";
 				const char *fName = prometheusBackupFile.toStdString().c_str();
 				if (fName)
 				{
